72623e38d09d81e0ef7a10cabf909b763c2a6ec7
Darya Sivitskaya <dara.sivitskaya@gmail.com>
2024-12-10 15:27:01 +0100

    feat: created routing structure, implemented basic controllers, added universal error handling; integrated routes in index.js and connected middleware for error handling in the main index.js

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,14 @@
+class DeviceController {
+    async create(req,res){
+
+    }
+    async getAll(req,res){
+        
+    }
+
+    async getOne(rec,res){
+        
+    }
+}
+
+module.exports = new DeviceController();
