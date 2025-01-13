de40f440a3b77a78d631b8f22b93be3f2996dd90
Darya Sivitskaya <dara.sivitskaya@gmail.com>
2025-01-13 15:34:54 +0100

    feat: implemented server integration and admin panel; added .env with API base URL; installed jwt-decode library; created htt p/index.js, http/userAPI.js, and http/deviceAPI.js for server interaction; implemented registration, login, and auth check via API; shifted product data storage to the database; updated components DevicePage, Shop, Auth, DeviceStore for API usage; added modals for creating brands, types, and devices in the admin panel; improved navigation and added user logout; corrected errors and typos in the server-side code.

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,33 +1,138 @@
-import React from 'react';
-import { Modal, Form, Button } from 'react-bootstrap';
+import React, {useContext, useState} from 'react';
+import Modal from "react-bootstrap/Modal";
+import {Button, Dropdown, Form, Row, Col} from "react-bootstrap";
+import {Context} from "../../index";
+import {observer} from "mobx-react-lite";
+import { createDevice } from '../../http/deviceAPI';
 
-const CreateDevice = ({show, onHide}) => {
+const CreateDevice = observer(({show, onHide}) => {
+    const {device} = useContext(Context)
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [file, setFile] = useState(null)
+    const [info, setInfo] = useState([])
+
+    const addInfo = () => {
+        setInfo([...info, {title: '', description: '', number: Date.now()}])
+    }
+    const removeInfo = (number) => {
+        setInfo(info.filter(i => i.number !== number))
+    }
+    const changeInfo = (key, value, number) => {
+        setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i))
+    }
+
+    const selectFile = e => {
+        setFile(e.target.files[0])
+    }
+
+    const addDevice = () => {
+        const formData = new FormData()
+        formData.append('name', name)
+        formData.append('price', `${price}`)
+        formData.append('img', file)
+        formData.append('brandId', device.selectedBrand.id)
+        formData.append('typeId',  device.selectedType.id)
+        formData.append('info', JSON.stringify(info))
+        createDevice(formData).then(data => onHide())
+    }
+
     return (
         <Modal
-        show={show}
-        onHide={onHide}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
+            show={show}
+            onHide={onHide}
+            centered
         >
-        <Modal.Header closeButton>
-        <Modal.Title id="contained-modal-title-vcenter">
-          Add a new device
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Form>
-            <Form.Control 
-                placeholder='Enter the device name'
-            />
-        </Form>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="outline-danger" onClick={onHide}>Close</Button>
-        <Button variant="outline-success" onClick={onHide}>Add</Button>
-      </Modal.Footer>
-    </Modal>
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Добавить устройство
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>{device.selectedType.name || "Выберите тип"}</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {device.types.map(type =>
+                                <Dropdown.Item
+                                    onClick={() => device.setSelectedType(type)}
+                                    key={type.id}
+                                >
+                                    {type.name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Dropdown className="mt-2 mb-2">
+                        <Dropdown.Toggle>{device.selectedBrand.name || "Выберите бренд"}</Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {device.brands.map(brand =>
+                                <Dropdown.Item
+                                    onClick={() => device.setSelectedBrand(brand)}
+                                    key={brand.id}
+                                >
+                                    {brand.name}
+                                </Dropdown.Item>
+                            )}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                    <Form.Control
+                        className="mt-3"
+                        onChange={e => setName(e.target.value)}
+                        placeholder="Введите название устройства"
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        onChange={e => setPrice(Number(e.target.value))}
+                        placeholder="Введите стоимость устройства"
+                        type="number"
+                    />
+                    <Form.Control
+                        className="mt-3"
+                        type="file"
+                        onChange = {selectFile}
+                    />
+                    <hr/>
+                    <Button
+                        variant={"outline-dark"}
+                        onClick={addInfo}
+                    >
+                        Добавить новое свойство
+                    </Button>
+                    {info.map(i =>
+                        <Row className="mt-4" key={i.number}>
+                            <Col md={4}>
+                                <Form.Control
+                                    value={i.title}
+                                    onChange={(e) => changeInfo('title', e.target.value, i.number)}
+                                    placeholder="Введите название свойства"
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <Form.Control
+                                    value={i.description}
+                                    onChange={(e) => changeInfo('description', e.target.value, i.number)}
+                                    placeholder="Введите описание свойства"
+                                />
+                            </Col>
+                            <Col md={4}>
+                                <Button
+                                    onClick={() => removeInfo(i.number)}
+                                    variant={"outline-danger"}
+                                >
+                                    Удалить
+                                </Button>
+                            </Col>
+                        </Row>
+                    )}
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
+                <Button variant="outline-success" onClick={addDevice}>Добавить</Button>
+            </Modal.Footer>
+        </Modal>
     );
-};
+});
 
 export default CreateDevice;
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,26 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Col, Row, Image, Card, Button } from 'react-bootstrap';
 import bigStar from '../assets/bigStar.png'
+import { useParams } from 'react-router-dom';
+import { fetchOneDevice } from '../http/deviceAPI';
 
 const DevicePage = () => {
-    const device =  {id: "1", name: "B310N", price: "50", rating: "0", img: "ad0aac0d-f9f5-41eb-80b8-fcd1c7d4cc2e.jpg"}
-    const description = [
-        { id: 1, title: "Тип подключения", description: "Беспроводное" },
-        { id: 2, title: "Шумоподавление", description: "Активное" },
-        { id: 3, title: "Время работы", description: "30 часов" },
-        { id: 4, title: "Микрофон", description: "Встроенный" },
-        { id: 5, title: "Вес", description: "254 грамма" }
-    ];
-    // const [device, setDevice] = useState({info: []})
-    // const {id} = useParams()
-    // useEffect(() => {
-    //     fetchOneDevice(id).then(data => setDevice(data))
-    // }, [])
+    const [device, setDevice] = useState({ info: [] });
+    const {id} = useParams()
+    useEffect(() => {
+        fetchOneDevice(id).then(data => setDevice(data))
+    }, [id, setDevice])
+
     return (
         <Container className='mt-3'>
               <Row>
                 <Col md={4}>
-                    <Image width={300} height={300} src={device.img}/>
+                    <Image width={300} height={300} src={process.env.REACT_APP_API_URL + device.img}/>
                 </Col>
                 <Col md={4}>
                     <div className="d-flex flex-column align-items-center">
@@ -45,7 +40,7 @@ const DevicePage = () => {
             </Row>
             <div className="d-flex flex-column m-3">
                 <h1>Характеристики</h1>
-                {description.map((info, index) => (
+                {device.info.map((info, index) => (
                     <Row 
                         key={info.id} 
                         style={{
